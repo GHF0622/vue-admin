1aebc8863672e45ba7d5057304bb0b0b2968c001
Ghf <guohaifei97@163.com>
2021-08-11 15:28:28 +0800

    首頁增加中國地圖

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,31 +1,294 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+  <div id="china_map_box">
+    <div id="china_map"></div>
   </div>
 </template>
 
 <script>
-
+import echarts from "echarts";
+import "echarts/map/js/china.js";
 export default {
-  name: 'Home',
-  data(){
-    return{
-      name:'4441'
-    }
+  data() {
+    return {
+      //echart 配制option
+      options: {
+        visualMap: {
+          //地图图例
+          show: true,
+          left: 26,
+          bottom: 40,
+          showLabel: true,
+          pieces: [
+            //根据数据大小，各省显示不同颜色
+            {
+              gte: 100,
+              label: ">= 1000",
+              color: "#1f307b",
+            },
+            {
+              gte: 500,
+              lt: 999,
+              label: "500 - 999",
+              color: "#3c57ce",
+            },
+            {
+              gte: 100,
+              lt: 499,
+              label: "100 - 499",
+              color: "#6f83db",
+            },
+            {
+              gte: 10,
+              lt: 99,
+              label: "10 - 99",
+              color: "#9face7",
+            },
+            {
+              lt: 10,
+              label: "<10",
+              color: "#bcc5ee",
+            },
+          ],
+        },
+        geo: {
+          //中国地图设置
+          map: "china",
+          scaleLimit: {
+            min: 1,
+            max: 2,
+          },
+          zoom: 1,
+          top: 120,
+          label: {
+            normal: {
+              show: true,
+              fontSize: "14",
+              color: "rgba(0,0,0,0.7)",
+            },
+          },
+          itemStyle: {
+            normal: {
+              borderColor: "rgba(0, 0, 0, 0.2)",
+            },
+            emphasis: {
+              areaColor: "#f2d5ad",
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              borderWidth: 0,
+            },
+          },
+        },
+        series: [
+          {
+            name: "突发事件",
+            type: "map",
+            geoIndex: 0,
+            data: [],
+          },
+        ],
+      },
+      //echart data
+      dataList: [
+        {
+          name: "南海诸岛",
+          value: 100,
+          eventTotal: 100,
+          specialImportant: 10,
+          import: 10,
+          compare: 10,
+          common: 40,
+          specail: 20,
+        },
+        {
+          name: "北京",
+          value: 540,
+        },
+        {
+          name: "天津",
+          value: 130,
+        },
+        {
+          name: "上海",
+          value: 400,
+        },
+        {
+          name: "重庆",
+          value: 750,
+        },
+        {
+          name: "河北",
+          value: 130,
+        },
+        {
+          name: "河南",
+          value: 830,
+        },
+        {
+          name: "云南",
+          value: 110,
+        },
+        {
+          name: "辽宁",
+          value: 19,
+        },
+        {
+          name: "黑龙江",
+          value: 150,
+        },
+        {
+          name: "湖南",
+          value: 690,
+        },
+        {
+          name: "安徽",
+          value: 60,
+        },
+        {
+          name: "山东",
+          value: 39,
+        },
+        {
+          name: "新疆",
+          value: 4,
+        },
+        {
+          name: "江苏",
+          value: 31,
+        },
+        {
+          name: "浙江",
+          value: 104,
+        },
+        {
+          name: "江西",
+          value: 36,
+        },
+        {
+          name: "湖北",
+          value: 52,
+        },
+        {
+          name: "广西",
+          value: 33,
+        },
+        {
+          name: "甘肃",
+          value: 7,
+        },
+        {
+          name: "山西",
+          value: 5,
+        },
+        {
+          name: "内蒙古",
+          value: 778,
+        },
+        {
+          name: "陕西",
+          value: 22,
+        },
+        {
+          name: "吉林",
+          value: 4,
+        },
+        {
+          name: "福建",
+          value: 18,
+        },
+        {
+          name: "贵州",
+          value: 5,
+        },
+        {
+          name: "广东",
+          value: 98,
+        },
+        {
+          name: "青海",
+          value: 1,
+        },
+        {
+          name: "西藏",
+          value: 0,
+        },
+        {
+          name: "四川",
+          value: 44,
+        },
+        {
+          name: "宁夏",
+          value: 4,
+        },
+        {
+          name: "海南",
+          value: 22,
+        },
+        {
+          name: "台湾",
+          value: 3,
+        },
+        {
+          name: "香港",
+          value: 5,
+        },
+        {
+          name: "澳门",
+          value: 555,
+        },
+      ],
+    };
   },
-  computed: {
-  }
-}
+  methods: {
+    //初始化中国地图
+    initEchartMap() {
+      console.log(echarts,'echarts');
+      let mapDiv = document.getElementById("china_map");
+      console.log(mapDiv,'mapDiv');
+      const myChart = echarts.init(mapDiv);
+      console.log(456);
+      myChart.setOption(this.options);
+    },
+    //修改echart配制
+    setEchartOption() {
+      this.options.series[0]["data"] = this.dataList;
+    },
+  },
+  created() {
+    this.setEchartOption();
+  },
+  mounted() {
+    this.$nextTick(() => {
+      console.log(12);
+      this.initEchartMap();
+    });
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
+<style scoped>
+#china_map_box {
+  height: 100%;
+  position: relative;
+}
+#china_map_box #china_map {
+  height: 80vh;
+}
+#china_map_box .china_map_logo {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 45px;
+}
+</style>
+<style>
+#china_map .tooltip_style {
+  line-height: 1.7;
+  overflow: hidden;
+}
+#china_map .tooltip_left {
+  float: left;
+}
+#china_map .tooltip_right {
+  float: right;
 }
 </style>
